9bd3af236db903059576acf9f40b637414f1f75b
Camilo LR <autumndrums@hotmail.com>
2025-05-01 18:01:35 -0300

    Configuração da conexão do banco de dados Postgre no projeto

--- src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
+++ src/main/java/br/com/techtaste/ms_pedidos/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.techtaste.ms_pedidos.controller;
 import br.com.techtaste.ms_pedidos.dto.PedidoRequestDto;
 import br.com.techtaste.ms_pedidos.dto.PedidoResponseDto;
 import br.com.techtaste.ms_pedidos.service.PedidoService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class PedidoController {
     private PedidoService service;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<PedidoResponseDto> cadastrarPedido(@RequestBody PedidoRequestDto pedidoDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.cadastrarPedido(pedidoDto));
     }
